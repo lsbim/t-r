65d53cecd6b72422fae64a702af845f78ba26487
lsbim <rnqjatnqja@gmail.com>
2025-11-27 11:47:49 +0900

    수정: 로딩 애니메이션 화면 정중앙에 가도록 수정

--- src/commons/component/Loading.tsx
+++ src/commons/component/Loading.tsx
@@ -13,7 +13,7 @@ const Loading = () => {
     return (
         <div className="relative flex flex-col justify-center gap-4 min-h-[100.5vh]" > {/* 스크롤을 위한 100.5vh */}
             <HeaderNav />
-            <div className="absolute flex items-center justify-center left-1/2">
+            <div className="absolute flex items-center gap-x-2 left-1/2 -translate-x-1/2">
                 {icons.map((src, index) => (
                     <img
                         key={`loading_image_${index}`}
