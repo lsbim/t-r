2050a7fc2949318fab57bf0cf9db752369bfc386
lsbim <rnqjatnqja@gmail.com>
2026-03-10 17:47:11 +0900

    추가: vite manualChunks 추가

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,36 @@ export default defineConfig(() => {
     return {
         build: {
             outDir: 'build',
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'vendor-react': ['react', 'react-dom'],
+                        'vendor-router': ['react-router-dom'],
+                        'vendor-query': ['@tanstack/react-query'],
+                        'vendor-chart': [
+                            'chart.js',
+                            'react-chartjs-2',
+                            'chartjs-plugin-annotation',
+                            'chartjs-plugin-datalabels',
+                        ],
+                        'vendor-motion': ['framer-motion'],
+                        'vendor-ui': [
+                            '@radix-ui/react-accordion',
+                            '@radix-ui/react-slider',
+                            'react-virtuoso',
+                            'react-tooltip',
+                            'react-helmet-async',
+                        ],
+                        'vendor-utils': ['es-hangul', 'es-toolkit'],
+                    },
+                },
+            },
         },
         plugins: [
             react(),
-
-        ]
+        ],
+        esbuild: {
+            drop: ['console', 'debugger'],
+        },
     };
 });
