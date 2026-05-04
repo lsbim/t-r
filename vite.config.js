2d7171c9ddad5df39bac1758062a62512e3039e5
lsbim <rnqjatnqja@gmail.com>
2026-05-05 08:25:47 +0900

    추가: Github Actions

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig(({ command }) => {
                     },
                 },
             },
+            sourcemap: false,
         },
         plugins: [
             react(),
