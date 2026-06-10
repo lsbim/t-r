ae8a5313f8888f55c25670f5067db10ae8163591
lsbim <rnqjatnqja@gmail.com>
2026-06-10 19:33:46 +0900

    refactor: SelectInfo 사도 사진 클릭 시 새 탭 페이지 이동

--- src/components/shared/select/SelectInfo.tsx
+++ src/components/shared/select/SelectInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { findPersonalityByName } from '../../../utils/function';
 import { charInfo } from '../../../data/trickcalChar';
+import { Link } from 'react-router-dom';
 
 interface SelectInfoProps {
     firstRank: number;
@@ -21,11 +22,15 @@ const SelectInfo: React.FC<SelectInfoProps> = ({
         <div className="w-full p-3 h-[200px] flex flex-col justify-center items-center gap-y-3 bg-white dark:bg-zinc-900 dark:border-zinc-700 rounded-xl border border-zinc-300">
             {/* 사진, 사도 명 */}
             <div className="flex gap-x-2 items-center w-full justify-center">
-                <div className={`overflow-hidden rounded-full w-14 h-14 border-4 border-${findPersonalityByName(select)}-dark`}>
+                <Link
+                    to={`/character/${select}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`overflow-hidden rounded-full w-14 h-14 border-4 border-${findPersonalityByName(select)}-dark`}>
                     <img
                         src={`/images/profile/${select.startsWith('우로스(') ? '우로스' : select}.webp`}
                     />
-                </div>
+                </Link>
                 <div className="flex flex-col min-w-[95px]">
                     <span className="font-bold truncate text-start text-[14px]">
                         {select === '시온' ? '시온 더 다크불릿' : select}
