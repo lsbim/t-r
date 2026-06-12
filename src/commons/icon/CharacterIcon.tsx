5186f792bf891065351fae7fc4be055c1b6a975c
lsbim <rnqjatnqja@gmail.com>
2026-06-12 19:21:06 +0900

    feat: CharacterIcon type mini 사진, 크기 변경하여 가시성 개선

--- src/commons/icon/CharacterIcon.tsx
+++ src/commons/icon/CharacterIcon.tsx
@@ -15,7 +15,7 @@ const CharacterIcon = ({
     const iconConfig = type === 'small'
         ? `aspect-[3/4] w-[60px] h-[80px] flex flex-col justify-center items-center ${personality === '공명' ? 'resonance-pers' : `bg-${personality}`}`
         : type === 'mini'
-            ? `w-[45px] z-20 h-[60px] border border-black flex flex-col justify-center items-center ${personality === '공명' ? 'resonance-pers' : `bg-${personality}`}`
+            ? `w-[52px] z-20 h-[60px] border border-black flex flex-col justify-center items-center ${personality === '공명' ? 'resonance-pers' : `bg-${personality}`}`
             : type === 'micro'
                 ? `w-[30px] z-20 h-[30px] border border-black flex flex-col justify-center items-center ${personality === '공명' ? 'resonance-pers' : `bg-${personality}`}`
                 : `aspect-[3/4] w-[90px] h-[120px] flex flex-col justify-center items-center ${personality === '공명' ? 'resonance-pers' : `bg-${personality}`}`;
@@ -23,7 +23,7 @@ const CharacterIcon = ({
     const nameConfig = type === 'small'
         ? "bg-white bg-opacity-80 font-bold w-full px-[2px] py-[2px] truncate text-[12px] z-10 flex items-center justify-center"
         : type === 'mini'
-            ? "bg-white bg-opacity-80 font-bold w-full px-[2px] py-1 truncate text-[11px] z-10 flex items-center justify-center"
+            ? "bg-white bg-opacity-80 font-bold w-full px-[2px] truncate text-[11px] min-h-[16px] z-10 flex items-center justify-center"
             : type === 'micro'
                 ? "hidden"
                 : "bg-white bg-opacity-80 font-bold w-full px-2 py-1 truncate text-[12px] z-10 flex items-center justify-center";
@@ -34,20 +34,24 @@ const CharacterIcon = ({
             ? '우로스'
             : name;
 
-        return type === 'small'
-            ? `/images/character/${charName}.webp`
-            : `/images/profile/${charName}.webp`
+        return type === 'micro'
+            ? `/images/profile/${charName}.webp`
+            : `/images/character/${charName}.webp`
     }
 
     return (
         <div
             data-tooltip-id="my-tooltip"
             data-tooltip-content={name}
-            className={iconConfig}>
-            <img
-                className={`h-full ${type === 'micro' ? 'object-cover' : 'object-contain'}`}
-                src={imgURL(name)}
-                decoding="async" />
+            className={`${iconConfig}`}>
+            <div
+                className={` overflow-hidden`}
+            >
+                <img
+                    className={`${type === 'mini' ? 'w-full object-cover object-top origin-top scale-[1.3]' : 'w-full'} ${type === 'micro' ? 'object-cover' : 'object-contain'}`}
+                    src={imgURL(name)}
+                    decoding="async" />
+            </div>
             <div className={nameConfig}>
                 <span className="truncate">
                     {name.startsWith('우로스') ? '우로스' : name}
