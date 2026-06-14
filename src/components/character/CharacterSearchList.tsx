e827129fba5dd5434a2907810879f434e295f697
lsbim <rnqjatnqja@gmail.com>
2026-06-14 18:40:03 +0900

    feat: CharacterSearchList, CostumeBoard 각 컴포넌트의 Input 스타일 통일

--- src/components/character/CharacterSearchList.tsx
+++ src/components/character/CharacterSearchList.tsx
@@ -15,15 +15,21 @@ const CharacterSearchList = () => {
                 <span className="font-bold">
                     사도 목록
                 </span>
-                <input
-                    type="text"
-                    placeholder="사도 검색(에르핀, ㅇㄹㅍ, ㅇㄿ)"
-                    className="w-[180px] outline-none border border-zinc-950 rounded-sm text-[12px] h-[30px] px-2 py-1 dark:bg-zinc-700"
-                    value={search}
-                    onChange={(e) => {
-                        setSearch(e.target.value);
-                    }}
-                />
+                <div className="relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                        className="size-3 text-gray-600 absolute pointer-events-none left-2 top-[9px] dark:text-zinc-200">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                    <input
+                        type="text"
+                        placeholder="사도 검색(에르핀, ㅇㄹㅍ, ㅇㄿ)"
+                        className="w-[200px] outline-none border border-zinc-950 rounded-sm text-[12px] h-[30px] pl-6 py-1 dark:bg-zinc-700 focus:border-orange-500 transition-[border] duration-250"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
+                    />
+                </div>
             </div>
             {/* 사도 목록 */}
             <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] p-4 gap-3 overflow-y-auto">
