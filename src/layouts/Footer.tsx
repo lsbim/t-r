642b5aee1adf907e40fe54349724357de5b1a579
lsbim <rnqjatnqja@gmail.com>
2026-08-17 11:46:53 +0900

    feat: Footer 데이터 오류 제보용 메일 추가

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,11 +1,17 @@
 
 const Footer = () => {
 
+    const first = 'rnqjatnqja'
+    const mail = `${first}@gmail.com`
+
     return (
-        <footer className="bg-white border-t border-zinc-300 dark:bg-zinc-900 gap-y-1 dark:text-zinc-200 dark:border-zinc-700 mt-auto h-[70px] w-full flex flex-col items-center justify-center p-2">
-            <small className="text-[10px] sm:text-[12px] ">
+        <footer className="bg-white border-t border-zinc-300 dark:bg-zinc-900 dark:text-zinc-200 dark:border-zinc-700 mt-auto h-[70px] w-full flex flex-col items-center justify-center p-2 text-[10px] sm:text-[12px] gap-y-2">
+            <span>
                 All data is retrieved from Trickcal, a game copyrighted by EPIDGames
-            </small>
+            </span>
+            <span>
+                버그 및 데이터 오류 제보: {mail}
+            </span>
         </footer>
     );
 }
